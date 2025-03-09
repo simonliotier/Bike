63fbe099dcb4f3570fc7ebab6e5ad3391b41059b
Simon Liotier <simon.liotier@iadvize.com>
2025-03-09 10:50:08 +0100

    Fix widget compilation issue on macOS

--- Widgets/Shared/Bike Battery/BikeBatteryWidgetAccessoryCornerView.swift
+++ Widgets/Shared/Bike Battery/BikeBatteryWidgetAccessoryCornerView.swift
@@ -8,11 +8,13 @@ struct BikeBatteryWidgetAccessoryCornerView: View {
     var body: some View {
         switch entry.state {
         case let .loaded(content):
+            #if !os(macOS)
             Text(content.batteryPercentage.formatted(.percent))
                 .widgetCurvesContent()
                 .widgetLabel {
                     ProgressView(value: content.batteryPercentage)
                 }
+            #endif
         case .error:
             Image(systemName: "exclamationmark.octagon.fill")
         }
